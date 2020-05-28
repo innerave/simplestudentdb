183743780b82bea6b5915a65785f37acdef94dfb
Alexander Vladimirov <innerave@gmail.com>
2020-05-28 03:28:56 +0300

    Изменение и удаление из базы
    Signed-off-by: Alexander Vladimirov <innerave@gmail.com>

--- routes/students.js
+++ routes/students.js
@@ -19,8 +19,19 @@ router.get('/student/new', (req,res)=> {
    res.render('new');
 });
 
+router.get('/edit/:id', (req, res)=> {
+    let searchQuery = {_id : req.params.id};
+    Student.findOne(searchQuery)
+        .then(student => {
+            res.render('edit', {student:student});
+        })
+        .catch(err => {
+            req.flash('error_msg', 'Ошибка: '+err)
+            res.redirect('/');
+        });
+});
 
-//POST
+//NEW
 router.post('/student/new', (req,res)=> {
    let newStudent = {
        name : req.body.name,
@@ -38,6 +49,40 @@ router.post('/student/new', (req,res)=> {
            req.flash('error_msg', 'Ошибка: '+err)
            res.redirect('/');
        });
+});
+
+//UPDATE
+router.put('/edit/:id', (req, res)=> {
+    let searchQuery = {_id : req.params.id};
+    Student.updateOne(searchQuery, {$set: {
+        name : req.body.name,
+        institute : req.body.institute,
+        department : req.body.department,
+        group : req.body.group
+    }})
+    .then(student => {
+        req.flash('success_msg', 'Информация о студенте обновлена успешно.')
+        res.redirect('/');
+    })
+    .catch(err => {
+        req.flash('error_msg', 'Ошибка: '+err)
+        res.redirect('/');
+    });
+});
+
+//DELETE
+router.delete('/delete/:id', (req, res)=> {
+    let searchQuery = {_id : req.params.id};
+
+    Student.deleteOne(searchQuery)
+        .then(student=>{
+            req.flash('success_msg', 'Студент успешно удален.')
+            res.redirect('/');
+        })
+        .catch(err => {
+            req.flash('error_msg', 'Ошибка: '+err)
+            res.redirect('/');
+        });
 });
 
 module.exports = router;
