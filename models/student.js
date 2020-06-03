de3967f7a7847f4274325b4afecd8f89a63f479c
Alexander Vladimirov <innerave@gmail.com>
2020-06-03 06:52:15 +0300

    Портфолио фронт

--- models/student.js
+++ models/student.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 
+let fileScheme = new mongoose.Schema({
+    file : String,
+});
+
 let studentScheme = new mongoose.Schema({
     name : String,
     institute : String,
     department : String,
-    group : String 
+    group : String,
+    portfolio : fileScheme
 });
 
 module.exports = mongoose.model('Student', studentScheme);
